7e0237a5b7e5540861289080de576d8a4359ae02
Zichong (Jason) Li <zichongli1998@gmail.com>
2021-02-11 00:31:25 -0800

    Display sample data component

---
+++ src/components/Data.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  playlistPicture: {
+    padding: '50px',
+    maxWidth: '400px',
+    maxHeight: '480px',
+  },
+});
+
+function Data({ playlistName, img, username, tracks }) {
+  const classes = useStyles();
+
+  if (img === '' || img === undefined) {
+    return <div></div>;
+  } else {
+    return (
+      <div>
+        Owner: {username} — Playlist Name: {playlistName}
+        <br></br>
+        <img
+          style={{}}
+          className={classes.playlistPicture}
+          src={img}
+          alt="blank"
+        />
+        <br></br>
+        Total Tracks: {tracks.length}
+      </div>
+    );
+  }
+}
+
+export default Data;
