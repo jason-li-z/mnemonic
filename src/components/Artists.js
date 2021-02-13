53e3e74ec168ad0dd51ac51a1aba6918cff42623
Zichong (Jason) Li <zichongli1998@gmail.com>
2021-02-13 00:51:49 -0800

    Simple Artist component to display every artist on the track

---
+++ src/components/Artists.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Artists({ artists }) {
+  // Artists == array
+
+  let artistName = '';
+  for (let i = 0; i < artists.length; i++) {
+    if (i === artists.length - 1) {
+      artistName += artists[i].name;
+    } else {
+      artistName += artists[i].name + ', ';
+    }
+  }
+
+  return <div>{artistName}</div>;
+}
+
+export default Artists;
