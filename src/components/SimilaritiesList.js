da20b99d4ebe48fd38ea14a67bcdec366c9c924f
Zichong (Jason) Li <zichongli1998@gmail.com>
2021-02-14 20:06:22 -0800

    added simple transitions

--- src/components/SimilaritiesList.js
+++ src/components/SimilaritiesList.js
@@ -7,52 +7,71 @@ import {
   ListItemAvatar,
   Avatar,
   Typography,
+  Zoom,
 } from '@material-ui/core';
 
 import Artists from './Artists';
 
+//<Artists artists={dups.track.artists}></Artists>
+
 function renderItem(item) {
   return item.map((dups) => (
-    <ListItem key={dups.track.id}>
-      <ListItemAvatar>
-        <Avatar
-          style={{ width: '100px', height: '100px', padding: '30px' }}
-          src={dups.track.album.images[1].url}
-          alt=""
-        ></Avatar>
-      </ListItemAvatar>
-      <ListItemText
-        primary={dups.track.name}
-        secondary={<Artists artists={dups.track.artists}></Artists>}
-      ></ListItemText>
-    </ListItem>
+    <Zoom in={true} timeout={1500}>
+      <ListItem key={dups.track.id}>
+        <ListItemAvatar>
+          <Avatar
+            style={{ width: '100px', height: '100px', padding: '30px' }}
+            src={dups.track.album.images[1].url}
+            alt=""
+          ></Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary={'Track Name: ' + dups.track.name}
+          secondary={<Artists artists={dups.track.artists}></Artists>}
+        ></ListItemText>
+      </ListItem>
+    </Zoom>
   ));
 }
 
-function SimilaritiesList({ tracks, duplicate, showResult }) {
+function SimilaritiesList({ tracks, duplicate, showResult, fade }) {
   if (!showResult) {
     return <div></div>;
   }
 
   if (tracks.length === 0) {
-    return <div>No duplicates were found</div>;
+    return (
+      <Zoom
+        in={fade}
+        timeout={400}
+        style={{ transitionDelay: true ? '500ms' : '0ms' }}
+      >
+        <div style={{ padding: '40px' }}>No duplicates were found</div>
+      </Zoom>
+    );
   }
 
   return (
-    <div>
-      <Typography variant="h5" style={{ paddingTop: '40px' }}>
-        found {tracks.length} similar tracks
-      </Typography>
-      <List
-        style={{
-          textAlign: 'center',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        {renderItem(tracks)}
-      </List>
-    </div>
+    <Zoom
+      in={fade}
+      timeout={400}
+      style={{ transitionDelay: true ? '500ms' : '0ms' }}
+    >
+      <div>
+        <Typography variant="h5" style={{ paddingTop: '40px' }}>
+          found {tracks.length} similar tracks
+        </Typography>
+        <List
+          style={{
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {renderItem(tracks)}
+        </List>
+      </div>
+    </Zoom>
   );
 }
 
