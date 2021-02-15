f05586ba74cd1a69e0a505a449f34d67e15e8f53
Zichong (Jason) Li <zichongli1998@gmail.com>
2021-02-14 20:07:02 -0800

    add in transition for the playlist information

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core/';
+import { Typography, Fade } from '@material-ui/core/';
 
 const useStyles = makeStyles({
   playlistPicture: {
@@ -10,22 +10,24 @@ const useStyles = makeStyles({
   },
 });
 
-function Data({ playlistName, img, username, tracks }) {
+function Data({ playlistName, img, username, tracks, fade, timeout }) {
   const classes = useStyles();
 
   if (img === '' || img === undefined) {
     return <div></div>;
   } else {
     return (
-      <div>
-        <Typography variant="h6" style={{ padding: '0 50px 0 50px' }}>
-          Owner: {username} — Playlist Name: {playlistName}
-        </Typography>
-        <br></br>
-        <img className={classes.playlistPicture} src={img} alt="blank" />
-        <br></br>
-        <Typography>Total Tracks: {tracks.length}</Typography>
-      </div>
+      <Fade in={fade} timeout={timeout}>
+        <div>
+          <Typography variant="h6" style={{ padding: '0 50px 0 50px' }}>
+            Owner: {username} — Playlist Name: {playlistName}
+          </Typography>
+          <br></br>
+          <img className={classes.playlistPicture} src={img} alt="blank" />
+          <br></br>
+          <Typography>Total Tracks: {tracks.length}</Typography>
+        </div>
+      </Fade>
     );
   }
 }
